94c839ddcb6afee0d5f8514508d7e5aaa36f6133
wonbin <wonbin3977@gmail.com>
2025-10-15 03:08:35 +0900

    feat(parser) : 기본 구분자 생성 및 사용자 입력 검증(올바른 형식인지) + 문자열 parsing 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import calculator.parser.Parser;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
@@ -7,6 +8,6 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println(input);
+        Parser.parse(input);
     }
 }
---
+++ src/main/java/calculator/parser/Parser.java
@@ -0,0 +1,39 @@
+package calculator.parser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Parser {
+    private static final ArrayList<String> defaultDelimeters = new ArrayList<>(List.of(",", ":"));
+
+    public static String[] parse(String input) {
+        List<String> delimeters = new ArrayList<>(defaultDelimeters);
+        if (input.startsWith("//")) { //으로 시작하는 단어 예외 처리
+            int newLineStartIndex = input.indexOf("\\n");
+            if (newLineStartIndex == -1) {
+                throw new IllegalArgumentException("잘못된 입력 형식");
+            }
+            String customDelimeter = input.substring(2, newLineStartIndex); //문자열이 2글자 이상이면 오류
+            if (customDelimeter.length() >= 2) {
+                throw new IllegalArgumentException("커스텀 구분자는 한 글자만 가능합니다.");
+            }
+            delimeters.add(customDelimeter);
+            input = input.substring(newLineStartIndex + 2); //입력 문자열 재설정
+        }
+        if (input.length() == 0) { //비어있는 경우
+            return new String[]{""};
+        }
+        if (!Character.isDigit(input.charAt(0))) {
+            throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+        }
+        String regex = delimeters.stream()
+                .map(Pattern::quote) // 특수문자 처리
+                .collect(Collectors.joining("|"));
+        String[] tokens = input.split(regex);
+        System.out.println(Arrays.toString(tokens));
+        return tokens;
+    }
+}
