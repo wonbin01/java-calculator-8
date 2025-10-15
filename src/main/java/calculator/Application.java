835d533aa5b4fb0f6f92865cd32e0a21850943af
wonbin <wonbin3977@gmail.com>
2025-10-16 00:37:52 +0900

    feat(main) : 빈 문자열이 입력된 경우, 결과값으로 0을 출력하고, 프로그램 종료되도록 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,10 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         String[] parsedString = Parser.parse(input); //구분자를 통해 나눠진 문자열
+        if (parsedString.length == 0) {
+            System.out.println("결과 : " + 0);
+            return;
+        }
         long sum = Stringadder.stringCalculator(parsedString);
         System.out.println("결과 : " + sum);
     }
