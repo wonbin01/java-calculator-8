1309f1c61ba014b4eaf0c02291fc5780835fbc8a
wonbin <wonbin3977@gmail.com>
2025-10-16 18:29:00 +0900

    refactor(Parser) : hasCustomerDelimeter 메서드 생성
    입력받은 문자열에 커스텀 문자가 있는지 확인하는 메서드 분리

--- src/main/java/calculator/parser/Parser.java
+++ src/main/java/calculator/parser/Parser.java
@@ -10,7 +10,7 @@ public class Parser {
 
     public static String[] parse(String input) {
         List<String> delimeters = new ArrayList<>(defaultDelimeters);
-        if (input.startsWith("//")) { //으로 시작하는 단어 예외 처리
+        if (hasCustomerDelimeter(input)) { //으로 시작하는 단어 예외 처리
             int newLineStartIndex = input.indexOf("\\n");
             if (newLineStartIndex == -1) {
                 throw new IllegalArgumentException("잘못된 입력 형식");
@@ -39,6 +39,10 @@ public class Parser {
         if (customDelimeter.length() >= 2) {
             throw new IllegalArgumentException("커스텀 구분자는 한 글자만 가능합니다.");
         }
+    }
+
+    private static boolean hasCustomerDelimeter(String input) {
+        return input.startsWith("//");
     }
 
     public static String[] removeBlank(String[] tokens) { //빈칸 제거하는 메서드
