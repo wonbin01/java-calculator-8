0810b6ed8360fb6156153f27139f6ac79dcea7e4
wonbin <wonbin3977@gmail.com>
2025-10-17 15:13:07 +0900

    refactor(calculator) : startCalculator 메서드 생성 및 클래스 분리
    main함수에서 로직 startCalculator 메서드로 분리
    
    calculator 클래스 생성 및 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,10 @@
 package calculator;
 
-import calculator.adder.StringAdder;
-import calculator.parser.Parser;
-import camp.nextstep.edu.missionutils.Console;
+import static calculator.calculation.Calculator.startCalculator;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        String[] parsedString = Parser.parse(input); //구분자를 통해 나눠진 문자열
-        if (parsedString.length == 0) {
-            System.out.println("결과 : " + 0);
-            return;
-        }
-        long sum = StringAdder.stringCalculator(parsedString);
-        System.out.println("결과 : " + sum);
+        startCalculator();
     }
 }
